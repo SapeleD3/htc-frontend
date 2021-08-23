7ba3f13e2898442cd7775913fb1c09a04219f983
sapeled3 <oyinkuromosesvictor@outlook.com>
2021-08-23 22:15:01 +0100

    auth

--- src/App.js
+++ src/App.js
@@ -2,27 +2,26 @@ import React from 'react';
 import { AuthenticatedUserApp } from './app/components/authenticatedApp';
 import { UnAuthenticatedUserApp } from './app/components/unAuthenticatedApp';
 import Loading from './components/full-page-loader';
+import firebase from 'firebase';
 
 const { useState, useEffect } = React;
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [user, setUser] = useState({});
   const [checkingStatus, setCheckingStatus] = useState(true);
 
   useEffect(() => {
-    const checkStatus = async () => {
-      try {
-        const status = false;
-        if (status) {
-          setIsLoggedIn(true);
-        }
-        setCheckingStatus(false);
-      } catch (error) {
-        setCheckingStatus(false);
-      }
-    };
-    checkStatus();
-  }, []);
+    // Handle user state changes
+    function onAuthStateChanged(user) {
+      setUser(user);
+      user && setIsLoggedIn(true);
+      if (checkingStatus) setCheckingStatus(false);
+    }
+
+    const subscriber = firebase.auth().onAuthStateChanged(onAuthStateChanged);
+    return subscriber;
+  }, [checkingStatus]);
 
   if (checkingStatus) {
     return <Loading />;
